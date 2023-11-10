6c28fc33fc18e8151b745b0f2d176a72bb83e17b
jjustesen <71100036+jjustesen@users.noreply.github.com>
2023-11-10 15:39:08 -0300

    refactor run

--- js/run.js
+++ js/run.js
@@ -208,23 +208,23 @@ function randomPiece() {
 // GAME LOOP
 //-------------------------------------------------------------------------
 
+function updateFrame(last, now) {
+  now = timestamp();
+  update(Math.min(1, (now - last) / 1000.0)); // using requestAnimationFrame have to be able to handle large delta's caused when it 'hibernates' in a background or non-visible tab
+  draw();
+  stats.update();
+  last = now;
+  requestAnimationFrame(frame, canvas);
+}
+
 function run() {
   showStats(); // initialize FPS counter
   addEvents(); // attach keydown and resize events
 
   var last = (now = timestamp());
   function frame() {
-    now = timestamp();
-    update(Math.min(1, (now - last) / 1000.0)); // using requestAnimationFrame have to be able to handle large delta's caused when it 'hibernates' in a background or non-visible tab
-    draw();
-    stats.update();
-    last = now;
-    requestAnimationFrame(frame, canvas);
+    updateFrame(last, now);
   }
-
-  resize(); // setup all our sizing information
-  reset(); // reset the per-game variables
-  frame(); // start the first frame
 }
 
 function showStats() {
